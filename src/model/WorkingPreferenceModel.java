aaa11c8d08a4f9c313dcdf31fe6ec76aae71237f
gsanthan <gsanthan@iastate.edu>
2012-10-16 16:09:59 -0500

    Support for reverse model
    Signed-off-by: gsanthan <gsanthan@iastate.edu>

--- src/model/WorkingPreferenceModel.java
+++ src/model/WorkingPreferenceModel.java
@@ -12,6 +12,16 @@ public class WorkingPreferenceModel {
 	 * PreferenceMetaData about the current preference specification used by the PreferenceReasoner.
 	 */
 	public static PreferenceMetaData prefMetaData = new PreferenceMetaData();
+	
+	public static PreferenceMetaData prefMetaDataReverse = new PreferenceMetaData();
+
+	public static PreferenceMetaData getPrefMetaDataReverse() {
+		return prefMetaDataReverse;
+	}
+
+	public static void setPrefMetaDataReverse(PreferenceMetaData prefMetaDataReverse) {
+		WorkingPreferenceModel.prefMetaDataReverse = prefMetaDataReverse;
+	}
 
 	public static PreferenceMetaData getPrefMetaData() {
 		return prefMetaData;
